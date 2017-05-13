e89fab042f8a6dc7d17c40d54e5299fdd842a8ac
Mohammad Haghighat <hosseinhg@hotmail.com>
2017-05-13 20:59:10 +0430

    Using subdomains

--- main.go
+++ main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+type (
+	Host struct {
+		Echo *echo.Echo
+	}
+)
+
+func main() {
+	hosts := map[string]*Host{}
+
+	// api
+	api := echo.New()
+	api.Use(middleware.Logger())
+	api.Use(middleware.Recover())
+
+	hosts["api.localhost:1323"] = &Host{api}
+
+	api.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "API")
+	})
+
+	// Server
+	e := echo.New()
+	e.Any("/*", func(c echo.Context) (err error) {
+		req := c.Request()
+		res := c.Response()
+		host := hosts[req.Host]
+
+		if host == nil {
+			err = echo.ErrNotFound
+		} else {
+			host.Echo.ServeHTTP(res, req)
+		}
+
+		return
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
