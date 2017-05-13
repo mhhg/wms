c4d75cc1fadea27d38c5660293273ad3a51b8eef
Mohammad Haghighat <hosseinhg@hotmail.com>
2017-05-13 21:34:04 +0430

    Common required utils

---
+++ common/utils.go
@@ -0,0 +1,63 @@
+package common
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+)
+
+type (
+	appError struct {
+		Error      string `json:"error"`
+		Message    string `json:"message"`
+		HttpStatus int    `json:"status"`
+	}
+	errorResource struct {
+		Data appError `json:"data"`
+	}
+	configuration struct {
+		Server      string
+		MongoDBHost string
+		DBUser      string
+		DBPwd       string
+		Database    string
+		LogLevel    int
+	}
+)
+
+func DisplayAppError(c echo.Context, handleError error, message string, code int) {
+	errObj := appError{
+		Error:      handlerError.Error(),
+		Message:    message,
+		HttpStatus: code,
+	}
+
+	Error.Printf("[AppError]: %s\n", handlerError)
+	if j, err := json.Marshal(errorResource{Data: errObj}); err == nil {
+		w.Write(j)
+	}
+}
+
+var AppConfig configuration
+
+func initConfig() {
+	loadAppConfig()
+}
+
+// Reads config.json and decode into AppConfig
+func loadAppConfig() {
+	file, err := os.Open("common/config.json")
+	defer file.Close()
+
+	if err != nil {
+		log.Fatalf("[loadConfig]: %s\n", err)
+	}
+
+	decoder := json.NewDecoder(file)
+	AppConfig = configuration{}
+	err = decoder.Decode(&AppConfig)
+	if err != nil {
+		log.Fatalf("[loadAppConfig]: %s\n", err)
+	}
+}
