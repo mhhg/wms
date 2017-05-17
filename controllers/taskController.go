73e330de7ead1f46caf9fb0b327215bf02960169
Mohammad Haghighat <hosseinhg@hotmail.com>
2017-05-17 14:50:25 +0430

    Task Controllers API

---
+++ controllers/taskController.go
@@ -0,0 +1,35 @@
+package controllers
+
+import (
+	"github.com/labstack/echo"
+)
+
+// CreateTask insert a new Task document
+// Handler for HTTP Post - "/tasks
+func CreateTask(c echo.Context) error {
+	return nil
+}
+
+// GetTasks returns all Task document
+// Handler for HTTP Get - "/tasks"
+func GetTasks(c echo.Context) error {
+	return nil
+}
+
+// GetTaskByID returns a single Task document by id
+// Handler for HTTP Get - "/tasks/:id"
+func GetTaskByID(c echo.Context) error {
+	return nil
+}
+
+// UpdateTask update an existing Task document
+// Handler for HTTP Put - "/tasks/:id"
+func UpdateTask(c echo.Context) error {
+	return nil
+}
+
+// DeleteTask deelete an existing Task document
+// Handler for HTTP Delete - "/tasks/:id"
+func DeleteTask(c echo.Context) error {
+	return nil
+}
