f53c1165a0310c90116018e3a5516b9d5d929980
Mohammad Haghighat <hosseinhg@hotmail.com>
2017-05-17 22:02:29 +0430

    Starting the implementation of routers package

--- routers/routes.go
+++ routers/routes.go
@@ -1,7 +1,28 @@
 package routers
 
-func Init() *echo.Echo {
-	e := echo.New()
+import (
+	"net/http"
 
-	return e
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/mhhg/wms/common"
+)
+
+// Init Entry point for setting routes
+func Init() map[string]*common.Host {
+
+	hosts := map[string]*common.Host{}
+
+	// api
+	api := echo.New()
+	api.Use(middleware.Logger())
+	api.Use(middleware.Recover())
+
+	hosts["api.localhost:8000"] = &common.Host{Echo: api}
+
+	api.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "API")
+	})
+
+	return hosts
 }
