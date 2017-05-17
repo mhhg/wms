6494b2a0f6094bf808beea304d56032cccff3bf1
Mohammad Haghighat <hosseinhg@hotmail.com>
2017-05-17 22:03:58 +0430

    Starting implementation of json api resource for models on http response

---
+++ controllers/resource.go
@@ -0,0 +1,12 @@
+package controllers
+
+import "github.com/mhhg/wms/models"
+
+//Models for JSON resources
+type (
+	// TaskResource For Post/Put - /tasks
+	// TaskResource For Get - /tasks/id
+	TaskResource struct {
+		Data models.Task `json:"data"`
+	}
+)
