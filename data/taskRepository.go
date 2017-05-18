ae4efeeab15d5466e19b67ec5ceeb910c8534094
Mohammad Haghighat <hosseinhg@hotmail.com>
2017-05-18 13:05:47 +0430

    Update task repository api and implement funcs

--- data/taskRepository.go
+++ data/taskRepository.go
@@ -1,30 +1,86 @@
 package data
 
 import (
-	"gopkg.in/mgo.v2"
 	"github.com/mhhg/wms/models"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
-type TaskRepository {
+// TaskRepository mongodb repository for Task
+type TaskRepository struct {
 	C *mgo.Collection
 }
 
+// Create insert a new task document
 func (r *TaskRepository) Create(task *models.Task) error {
-	
+	task.ID = bson.NewObjectId()
+	err := r.C.Insert(
+		bson.M{
+			"name":         task.Name,
+			"no":           task.No,
+			"description":  task.Description,
+			"flow_id":      task.FlowID,
+			"po_id":        task.PoID,
+			"type":         task.Type,
+			"status":       task.Status,
+			"value":        task.Value,
+			"priority":     task.Priority,
+			"assign_type":  task.AssignType,
+			"quantity":     task.Quantity,
+			"line":         task.Line,
+			"shipment_num": task.ShipmentNum,
+			"unit":         task.Unit,
+			"sub_total":    task.SubTotal,
+			"start_date":   task.StartDate,
+			"end_date":     task.EndDate,
+		})
+	return err
 }
 
+// Update a task document
 func (r *TaskRepository) Update(task *models.Task) error {
-
+	err := r.C.UpdateId(task.ID,
+		bson.M{"$set": bson.M{
+			"name":         task.Name,
+			"no":           task.No,
+			"description":  task.Description,
+			"flow_id":      task.FlowID,
+			"po_id":        task.PoID,
+			"type":         task.Type,
+			"status":       task.Status,
+			"value":        task.Value,
+			"priority":     task.Priority,
+			"assign_type":  task.AssignType,
+			"quantity":     task.Quantity,
+			"line":         task.Line,
+			"shipment_num": task.ShipmentNum,
+			"unit":         task.Unit,
+			"sub_total":    task.SubTotal,
+			"start_date":   task.StartDate,
+			"end_date":     task.EndDate,
+		}})
+	return err
 }
 
-func (r *TaskRepository) GetAll(task *models.Task) error {
-
+// Delete a task document by id
+func (r *TaskRepository) Delete(id string) error {
+	err := r.C.Remove(bson.M{"_id": bson.ObjectIdHex(id)})
+	return err
 }
 
-func (r *TaskRepository) GetById(task *models.Task) error {
-
+// GetAll get all tasks
+func (r *TaskRepository) GetAll() []models.Task {
+	var tasks []models.Task
+	iter := r.C.Find(nil).Iter()
+	result := models.Task{}
+	for iter.Next(&result) {
+		tasks = append(tasks, result)
+	}
+	return tasks
 }
 
-func (r *TaskRepository) DeleteById(task *models.Task) error {
-	
+// GetByID return task document by id
+func (r *TaskRepository) GetByID(id string) (task models.Task, err error) {
+	err = r.C.FindId(bson.ObjectIdHex(id)).One(&task)
+	return
 }
